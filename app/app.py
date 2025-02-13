ba3fefd7d45f617dd26bcff4f627494c549f85d5
Francisco José Fernández Guardia <93257108+ffguardia@users.noreply.github.com>
2025-02-13 09:14:01 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from main import register_blueprints
+from db import init_app
 
-# Application initializations
+# Crear la instancia de la aplicación
 app = Flask(__name__)
 
-# settings
+# Configuración de la clave secreta
 app.secret_key = "mysecretkey"
+
+# Inicializar la base de datos
+init_app(app)
+
+# Registrar blueprints
+register_blueprints(app)
+
+# Iniciar la aplicación
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
